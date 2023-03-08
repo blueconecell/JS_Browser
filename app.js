8f16ecb55af39cbcf0813f6edaca377c6954bff5
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-03-08 22:00:49 +0900

    4.6

--- app.js
+++ app.js
@@ -3,14 +3,27 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
   event.preventDefault();
   loginForm.classList.add(HIDDEN_CLASSNAME);
   const username = loginInput.value;
-  localStorage.setItem("username", username);
+  localStorage.setItem(USERNAME_KEY, username);
+  paintGreetings(username);
+}
+function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+const savedUseraname = localStorage.getItem(USERNAME_KEY);
+
+if (savedUseraname === null) {
+  // form
+  loginForm.classList.remove(HIDDEN_CLASSNAME);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  // greetings
+  paintGreetings(savedUseraname);
+}
