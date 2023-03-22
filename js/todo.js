1065ba97d29c22f3c3e5311359236497d7b118bf
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-03-22 22:27:56 +0900

    7.6

--- js/todo.js
+++ js/todo.js
@@ -1,13 +1,13 @@
 const toDoForm = document.getElementById("todo-form");
-const toDoInput = toDoForm.querySelector("input");
+const toDoInput = document.querySelector("#todo-form input");
 const toDoList = document.getElementById("todo-list");
 
 const TODOS_KEY = "todos";
 
 let toDos = [];
 
-function saveToDos(){
-    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+function saveToDos() {
+  localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
 
 function deleteToDo(event){
@@ -15,25 +15,30 @@ function deleteToDo(event){
     li.remove();
 }
 
-function paintToDo(newTodo){
-    const li = document.createElement("li");
-    const span = document.createElement("span");
-    const button = document.createElement("button");
-    button.innerText = '❌';
-    button.addEventListener("click", deleteToDo);
-    li.appendChild(span);
-    li.appendChild(button);
-    span.innerText = newTodo;
-    toDoList.appendChild(li);
+function paintToDo(newTodo) {
+  const li = document.createElement("li");
+  li.id = newTodo.id;
+  const span = document.createElement("span");
+  span.innerText = newTodo.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
+  li.appendChild(span);
+  li.appendChild(button);
+  toDoList.appendChild(li);
 }
-
 
 function handleToDoSubmit(event){
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo)
-    paintToDo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    };
+    console.log(newTodoObj.text, newTodo);
+    toDos.push(newTodoObj);
+    paintToDo(newTodoObj);
     saveToDos();
 
 }
@@ -41,7 +46,9 @@ toDoForm.addEventListener("submit", handleToDoSubmit);
 
 const savedToDos = localStorage.getItem(TODOS_KEY);
 
- if(saveToDos){
+ if(savedToDos !== null){
     const parsedToDos = JSON.parse(savedToDos);
-    parsedToDos.forEach(paintToDo); //익명함수 넣거나 자체 만든 함수를 그냥 넣거나 
+    //익명함수 넣거나 자체 만든 함수를 그냥 넣거나 
+    toDos=parsedToDos;
+    parsedToDos.forEach(paintToDo);
  }
