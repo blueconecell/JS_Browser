c5569758039d0228748a9f07dfd3382b51321a13
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-03-29 21:56:51 +0900

    bg

--- js/background.js
+++ js/background.js
@@ -7,6 +7,6 @@ const bgImage = document.createElement("img");
 
 bgImage.src = `img/${chosenImage}`
 bgImage.id = "bgImage"
-document.body.style.backgroundImage = `linear-gradient( rgba(0,0,0,0.7 ),rgba(0,0,0,0.1) ), url(${bgImage.src})`
+document.body.style.backgroundImage = `url(${bgImage.src})`
 console.log(`url(${bgImage.src})`)
 // document.body.appendChild(bgImage)
