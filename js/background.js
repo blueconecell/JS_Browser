f40585cb515be6ff648fe3cae113696dd913fd5c
JeongYeonKim <95961799+blueconecell@users.noreply.github.com>
2023-03-29 16:48:33 +0900

    background

--- js/background.js
+++ js/background.js
@@ -4,6 +4,9 @@ const chosenImage = images[Math.floor(Math.random() * images.length)];
 
 const bgImage = document.createElement("img");
 
-bgImage.src = `img/${chosenImage}`
 
-document.body.appendChild(bgImage)
+bgImage.src = `img/${chosenImage}`
+bgImage.id = "bgImage"
+document.body.style.backgroundImage = `linear-gradient( rgba(0,0,0,0.7 ),rgba(0,0,0,0.1) ), url(${bgImage.src})`
+console.log(`url(${bgImage.src})`)
+// document.body.appendChild(bgImage)
